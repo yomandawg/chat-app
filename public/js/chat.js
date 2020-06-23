d1c977ca177222b472872fe87509272375b9ed1b
Ray Kim <yomandawgshit@gmail.com>
2020-06-24 00:39:37 +0900

    working with templates and location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,22 +1,73 @@
 const socket = io() // the client is able to connect to WebSocket from `/socket.io/socket.io.js` statement in html
 
-// socket.on('countUpdated', (count) => {
-//   console.log('The count has been updated', count)
-// })
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
 
-// document.querySelector('#increment').addEventListener('click', () => {
-//   console.log('Clicked')
-//   socket.emit('increment')
-// })
-
-document.querySelector('#message-form').addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  const message = e.target.elements.message.value
-
-  socket.emit('sendMessage', message)
-})
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMesssageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
   console.log(message)
+  const html = Mustache.render(messageTemplate, {
+    // pass data to Mustache object in HTML
+    message
+  })
+  $messages.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  // disable
+  $messageFormButton.setAttribute('disabled', 'disabled')
+
+
+  const message = e.target.elements.message.value
+
+  socket.emit('sendMessage', message, /* run when the event is acknowledged */ (error) => {
+    // enable
+    $messageFormButton.removeAttribute('disabled')
+    $messageFormInput.value = ''
+    $messageFormInput.focus()
+
+    if (error) {
+      return console.log(error)
+    }
+
+    console.log('Message delivered!')
+  })
+})
+
+socket.on('locationMessage', (url) => {
+  console.log(url)
+  const html = Mustache.render(locationMesssageTemplate, {
+    url
+  })
+  $messages.insertAdjacentHTML('beforeend', html)
+})
+
+$sendLocationButton.addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.')
+  }
+
+  // disable
+  $sendLocationButton.setAttribute('disabled', 'disabled')
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, () => {
+      // enable
+      $sendLocationButton.removeAttribute('disabled')
+
+      console.log('Location shared!')
+    })
+  })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 /*
@@ -36,8 +37,20 @@ io.on('connection', (socket) => {
   socket.emit('message', 'Welcome!')
   socket.broadcast.emit('message', 'A new user has joined!')
 
-  socket.on('sendMessage', (message) => {
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter()
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!')
+    }
+
     io.emit('message', message)
+    callback()
+  })
+
+  socket.on('sendLocation', (coords, callback) => {
+    io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    callback()
   })
 
   socket.on('disconnect', () => {
